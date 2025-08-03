76742ea22107675b502d8911aa6d90d1b1da0fab
Maherin-Mathakiya <mathakiyamaherin10@gmail.com>
2025-08-03 11:15:13 +0530

    Add files via upload

---
+++ a1p1.cpp
@@ -0,0 +1,13 @@
+//wap to find the addition of two numbers
+#include<stdio.h>
+int main()
+{
+	int n1,n2,sum;
+	printf("enter the first number");
+	scanf("%d",&n1);
+	printf("enter the second number");
+	scanf("%d",&n2);
+	sum=n1+n2;
+	printf("sum=%d",sum);
+	return 0;
+}
